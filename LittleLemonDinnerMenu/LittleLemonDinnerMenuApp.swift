3fd92a72266b18236e42334dd20fb3940e95f29a
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-09-29 17:51:42 +0700

    Initial Commit

---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenuApp.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
